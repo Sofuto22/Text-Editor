445a598802293673042459fc8f83c9dd767f64f1
Sofuto22 <zenigata19@gmail.com>
2023-02-10 04:53:27 -0500

    set asset caching

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,14 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  ({request}) => ["style", "script"].includes(request),
+  new offlineFallback({
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses[0, 200],
+      })
+    ] 
+    })
+   
+);
